df8505349705e2b7af913451406ffcaed29216f0
Elliot Woods <elliot@kimchiandchips.com>
2023-08-12 21:11:22 +0900

    changes to messaging interface

--- src/msgpack/Messaging.hpp
+++ src/msgpack/Messaging.hpp
@@ -10,9 +10,11 @@ namespace msgpack {
 		virtual const char * getTypeName() const = 0;
 	
 		virtual void reportStatus(Serializer &) = 0;
-		bool processIncoming(Stream &);
+
+		// If you do not override this, then you need to override processIncomingByKey below
+		virtual bool processIncoming(Stream &);
 		bool matchesKey(const char * key);
 	protected:
-		virtual bool processIncomingByKey(const char * key, Stream &) = 0;
+		virtual bool processIncomingByKey(const char * key, Stream &);
 	};
 }
